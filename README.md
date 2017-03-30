dade9134b3ff62330273be2b88510e1d6d49e88c
crlsndrsjmnz <crlsndrsjmnz@gmail.com>
2017-03-30 21:00:45 +0200

    Adding attribution

---
+++ README.md
@@ -0,0 +1,5 @@
+The code in this example is mostly taken from the examples provided by Google on the
+following links:
+
+https://developer.android.com/training/camera/index.html
+https://developer.android.com/training/camera/photobasics.html
--- app/src/main/java/co/carlosandresjimenez/android/myshareimageexample/MainActivity.java
+++ app/src/main/java/co/carlosandresjimenez/android/myshareimageexample/MainActivity.java
@@ -24,6 +24,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+/*
+ * The code in this example is mostly taken from the examples provided by Google on the
+ * following links:
+ *
+ * https://developer.android.com/training/camera/index.html
+ * https://developer.android.com/training/camera/photobasics.html
+ */
 
 public class MainActivity extends AppCompatActivity {
 
